24d85b516970d7ddfd31949aa7808bb4d6b7411a
Aluno SENAI <Aluno@SN-358646.edu.fiesc.com.br>
2024-07-05 12:37:29 -0300

    subindo primeira parte de JS

--- formulario.js
+++ formulario.js
@@ -1,1 +1,20 @@
+let botão =  document.getElementById ('button')
 
+
+function Verificação() {
+   
+    let emailUser = document. getElementById ('email').value
+    let userUser = document. getElementById ('user').value
+    let senhaUser = document. getElementById ('senha').value
+    let telUser = document. getElementById ('tel').value
+    let Usernasc = document. getElementById ('nasc').value
+
+    let idadeUser = document. querySelector ("input[name = 'idade']:checked").value
+
+    alert('Seu email é: ' + emailUser + 
+    '\n seu usuário é: ' + userUser + 
+    '\n sua senha é: ' + senhaUser + 
+    '\n seu telefone é: ' + telUser +
+     '\n sua data de nascimento é: ' + Usernasc
+     + '\n sua faixa etária é do grupo: ' + idadeUser)
+}
